3be4b9c6b341e7df3f9ad7b954dfc38ecf8c27c5
Erick Hitter <git-contrib@ethitter.com>
2017-06-27 19:17:05 -0700

    Radically simplified, plus an init script!

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Pipe logs to Mattermost or Slack webhooks
 
 # Requirements
 * Mattermost or Slack instance
-* `tail` and `curl` in `$PATH`
+* `curl`, `jq`, `tail`
 
-## TODO
-
-* Config file(s)
-* Init script with defaults override, restart, etc
-* Make binary deps paths configurable?
+# Installation
+1. `git clone https://git.ethitter.com/debian/eth-log-alerting.git /usr/local/eth-log-alerting`
+2. `cp /usr/local/eth-log-alerting/init.sh /etc/init.d/eth_log_alerting`
+3. `cp /usr/local/eth-log-alerting/defaults /etc/default/eth_log_alerting`
+4. Edit `/etc/default/eth_log_alerting`
+5. `sudo update-rc.d eth_log_alerting defaults`
+6. `/etc/init.d/eth_log_alerting start`
--- pipe.sh
+++ pipe.sh
@@ -1,3 +1,28 @@
-#!/bin/sh
-# TODO: path and config for send.sh
-/usr/bin/tail -Fq log.log | while read x ; do send.sh "$x" "#errors" "danger"; done
+#!/bin/bash
+
+# Defaults and aliases
+LOG_FILE=$1
+WEBHOOK_URL=$2
+USERNAME=${3:-logbot}
+CHANNEL=${4:-#logs}
+COLOR=${5:-default}
+ICON_URL=$6
+GREP=$7
+
+tail -n0 -F "$LOG_FILE" | while read LINE; do
+    (echo "$LINE" | grep -e "$GREP") && jq -n --arg line_encoded "    $LINE"  \ "{ \
+        channel: \"$CHANNEL\", \
+        username: \"$USERNAME\", \
+        attachments: [ { \
+            fallback: \"New entry in $LOG_FILE\", \
+            pretext: \"\`$LOG_FILE\`\", \
+            text: \$line_encoded, \
+            color: \"$COLOR\" \
+        } ],\
+        icon_url: \"$ICON_URL\" \
+    }" | cat <(echo "payload=") <(cat -) | curl \
+    -X POST \
+    -s \
+    -d@- \
+    $WEBHOOK_URL;
+done
